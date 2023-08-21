563f45fb70751d85efad2eb36cef3c82be1e67f9
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2023-08-21 11:02:10 -0700

    _printf.c

---
+++ main.h
@@ -0,0 +1,49 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * struct format - this matches the conversion specifiers for printf
+ *
+ * @id: char pointer of the specifier
+ * @f: pointer to function for the .
+ */
+
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+
+int _printf(const char *format, ...);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+
+#endif /* MAIN_H */
